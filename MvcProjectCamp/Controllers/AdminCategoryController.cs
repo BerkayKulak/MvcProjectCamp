d5f47c516f66de1c2c24c593e283d2e0d49a63de
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-07 22:32:01 +0300

    Temanın Eklenmesi

---
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AdminCategoryController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
