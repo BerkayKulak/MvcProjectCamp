da2fd21aa71e2da9abb3a698aacfc0ade898988d
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-08 22:12:08 +0300

    Mesajların Listelenmesi

--- MvcProjectCamp/Controllers/ContactController.cs
+++ MvcProjectCamp/Controllers/ContactController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 
 namespace MvcProjectCamp.Controllers
 {
     public class ContactController : Controller
     {
         // GET: Contact
-
+        private ContactManager cm = new ContactManager(new EfContactDal());
+        private ContactValidator cv = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contactValues = cm.GetList();
+            return View(contactValues);
         }
     }
 }
