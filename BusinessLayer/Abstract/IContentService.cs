17363d717a1456036d20da8d2532253171ddd97e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-08 19:53:23 +0300

    Content Controller

---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        List<Content> GetListById(int id);
+        void ContentAddBL(Content category);
+        Content GetById(int id);
+        void ContentDelete(Content category);
+        void ContentUpdate(Content category);
+    }
+}
