b8205742a7b255f1b406792f6879b12c62efc715
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-08 13:27:20 +0300

    Başlık İşlemleri

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager:IHeadingService
+    {
+        private IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+
+        public void HeadingAddBL(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public Heading GetById(int id)
+        {
+            return _headingDal.Get(x => x.HeadingId == id);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
