51dee3cdecb266ded23aa9b91cda70a5c5bf6e5a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-08 23:03:33 +0300

    Mesaj Controller Ve Admine Gelen Mesajlar

---
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetList();
+        void MessageAddBL(Message message);
+        Message GetById(int id);
+        void MessageDelete(Message message);
+        void MessageUpdate(Message message);
+    }
+}
---
+++ MvcProjectCamp/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        private MessageManager mm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetList();
+            return View(messageList);
+        }
+    }
+}
