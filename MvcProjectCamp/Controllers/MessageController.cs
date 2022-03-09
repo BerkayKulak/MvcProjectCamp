4f692dac38e2aa524d316415da9705c6b644a626
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-09 19:21:23 +0300

    Yeni Mesaj Sayfası

--- MvcProjectCamp/Controllers/MessageController.cs
+++ MvcProjectCamp/Controllers/MessageController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 
 namespace MvcProjectCamp.Controllers
 {
@@ -12,10 +13,30 @@ namespace MvcProjectCamp.Controllers
     {
         // GET: Message
         private MessageManager mm = new MessageManager(new EfMessageDal());
+
         public ActionResult Inbox()
         {
-            var messageList = mm.GetList();
+            var messageList = mm.GetListInbox();
             return View(messageList);
+        }
+
+        public ActionResult Sendbox()
+        {
+            var messageList = mm.GetListSendbox();
+            return View(messageList);
+        }
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+
+            return View();
         }
     }
 }
