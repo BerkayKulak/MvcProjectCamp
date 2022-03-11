6240f3b012169336af70dcec31c0a049910b924a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-11 13:26:05 +0300

    Yetkilendirme Düzenlemeleri

---
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private AdminManager admin = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminValues = admin.GetList();
+            return View(adminValues);
+        }
+    }
+}
