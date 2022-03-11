1e12fc55dfa5c1c0653174165f5838a66e14476c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-03-11 12:50:08 +0300

    Google Chartlar

---
+++ MvcProjectCamp/Controllers/ChartController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityLayer.Concrete;
+using MvcProjectCamp.Models;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+
+            ct.Add(new CategoryClass()
+            {
+                CategoryCount = 8,
+                CategoryName = "Yazılım"
+
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryCount = 5,
+                CategoryName = "Seyahat"
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryCount = 7,
+                CategoryName = "Teknoloji"
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryCount = 2,
+                CategoryName = "Spor"
+            });
+            return ct;
+        }
+    }
+}
